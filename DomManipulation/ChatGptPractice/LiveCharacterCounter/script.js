1bb976eb242d27242d0cb31ffd5edd8d7e363272
Gyanendra Aryal <gyanendraaryal4@gmail.com>
2025-06-18 14:43:17 +0545

    Initial commi

--- DomManipulation/ChatGptPractice/LiveCharacterCounter/script.js
+++ DomManipulation/ChatGptPractice/LiveCharacterCounter/script.js
@@ -1,3 +1,12 @@
-let txt = document.querySelector("#id");
-let count=0;
-txt.addEventListener("ket")
+const txt = document.querySelector("#txt");
+const charcount=document.querySelector("#char-count");
+txt.addEventListener('input',()=>{
+    const count=txt.value.length;
+    charcount.innerText=`Character ${count}`;
+    if(count>100){
+        document.body.style.backgroundColor="red";
+        charcount.innerText= `Warning!! You exceeded your limits. ${count}`;
+    }else{
+        document.body.style.backgroundColor="white";
+    }
+})
